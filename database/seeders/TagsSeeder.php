c4c21e6a368c4d2bbe762cc245e29663b501c42e
Fredericld <fred@rfmedia.co.uk>
2020-11-27 15:13:20 +0000

    Seeder updated

--- database/seeders/TagsSeeder.php
+++ database/seeders/TagsSeeder.php
@@ -30,6 +30,48 @@ class TagsSeeder extends Seeder
 
         }
 
+
+        $TagsYearGroupsTags= [7, 8, 9, 10, 11, 12, 13, 'post'];
+        foreach($TagsYearGroupsTags as $TagsYearGroupsTag) {
+            SystemTag::create([
+                'type' => 'year',
+                'name' => $TagsYearGroupsTag,
+                'live' => 'Y'
+            ]);
+        }
+        $this->command->info('Year Groups Tags created!');
+
+
+
+        $LscsTags= ['1-2', '2-3', '3-4', '4-5'];
+        foreach($LscsTags as $LscsTag) {
+            SystemTag::create([
+                'type' => 'year',
+                'name' => $LscsTag,
+                'live' => 'Y'
+            ]);
+        }
+        $this->command->info('Life Stage Careers Score LSCS Tags created!');
+
+
+        SystemTag::create([
+            'type' => '',
+            'name' => 'High Priority',
+            'live' => 'Y'
+        ]);
+
+        SystemTag::create([
+            'type' => '',
+            'name' => 'Red flag',
+            'live' => 'Y'
+        ]);
+
+        SystemTag::create([
+            'type' => '',
+            'name' => 'Report to users profile',
+            'live' => 'Y'
+        ]);
+
         $this->command->info('Tags Table seeded!');
 
     }
