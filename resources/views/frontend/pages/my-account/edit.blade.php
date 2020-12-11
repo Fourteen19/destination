fe9ea7ed523e7527c90306e0c0b6fd37c7a04503
Rick Garnett <rick@rfmedia.co.uk>
2020-12-11 16:04:26 +0000

    111220 Work on my account area

--- resources/views/frontend/pages/my-account/edit.blade.php
+++ resources/views/frontend/pages/my-account/edit.blade.php
@@ -1,5 +1,30 @@
 @extends('frontend.layouts.loggedin')
 
 @section('content')
-    my account
+<div class="row">
+    <div class="col-lg-2">
+    <div class="sticky-p position-sticky pr-lg-4 mr-lg-4">
+        <ul class="list-unstyled py-3">			
+            <li class="mb-2"><a href="#overview" data-scroll="" class="t-up t-wg w900 t16 td-no">Overview</a></li>
+            <li class="mb-2"><a href="#location" data-scroll="" class="t-up t-wg w900 t16 td-no">Location</a></li>
+            <li class="mb-2"><a href="#gallery" data-scroll="" class="t-up t-wg w900 t16 td-no">Gallery</a></li>
+            <li class="mb-2"><a href="#promotion" data-scroll="" class="t-up t-wg w900 t16 td-no">Promotion &amp; Engagement</a></li>
+            <li class="mb-2"><a href="#masterplans" data-scroll="" class="t-up t-wg w900 t16 td-no">Masterplans</a></li>
+            <li class="mb-2"><a href="#sales" data-scroll="" class="t-up t-wg w900 t16 td-no">Sale</a></li>								
+        </ul>
+    </div>
+    </div>
+    <div class="col-lg-9">
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+    </div>
+</div>
+  
+
+
 @endsection
