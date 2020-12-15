4fc80b41e43c8f0ae1d9502a5edcd8fde3105799
Rick Garnett <rick@rfmedia.co.uk>
2020-12-15 09:39:44 +0000

    My Account page setup

--- resources/views/frontend/pages/my-account/edit.blade.php
+++ resources/views/frontend/pages/my-account/edit.blade.php
@@ -1,27 +1,61 @@
 @extends('frontend.layouts.loggedin')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-2">
-    <div class="sticky-p position-sticky pr-lg-4 mr-lg-4">
-        <ul class="list-unstyled py-3">			
-            <li class="mb-2"><a href="#overview" data-scroll="" class="t-up t-wg w900 t16 td-no">Overview</a></li>
-            <li class="mb-2"><a href="#location" data-scroll="" class="t-up t-wg w900 t16 td-no">Location</a></li>
-            <li class="mb-2"><a href="#gallery" data-scroll="" class="t-up t-wg w900 t16 td-no">Gallery</a></li>
-            <li class="mb-2"><a href="#promotion" data-scroll="" class="t-up t-wg w900 t16 td-no">Promotion &amp; Engagement</a></li>
-            <li class="mb-2"><a href="#masterplans" data-scroll="" class="t-up t-wg w900 t16 td-no">Masterplans</a></li>
-            <li class="mb-2"><a href="#sales" data-scroll="" class="t-up t-wg w900 t16 td-no">Sale</a></li>								
-        </ul>
-    </div>
-    </div>
+<div class="row p-w">
+    @include('frontend.pages.includes.account-menu')
     <div class="col-lg-9">
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
-        <p>Consectetur aute nisi eiusmod minim eu ut sit deserunt consequat excepteur tempor. Est qui in sunt in est cupidatat culpa mollit nisi consequat sint duis laborum quis. Esse consequat ullamco adipisicing reprehenderit sit irure exercitation qui eiusmod id ad proident amet eu. Nostrud magna reprehenderit Lorem id tempor enim magna cillum ex veniam duis. Ex culpa est nisi do mollit duis nisi ut anim sunt in Lorem. In enim fugiat proident enim occaecat exercitation excepteur aliquip in aliqua esse.</p>
+        <div class="border-left ml-lg-4 pl-lg-5 def-border">
+            <h1 class="t36 fw700 mb-4">Edit my details</h1>
+            <p>You can use the form below to update your details. Not all of your details can be edited. If any of the information is incorrect then you can <a href="{{ route('frontend.my-account.contact-my-adviser.edit') }}">contact your adviser</a> or your school to request that they are changed.</p>
+            <div class="row">
+                <div class="col">
+                    <div class="border-top def-border w-100 mb-5"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <form>
+                    <div class="form-group">
+                        <label for="Firstname">First name</label>
+                        <input type="text" class="form-control form-control-lg" id="Firstname" placeholder="[Users First Name]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="Surname">Surname</label>
+                        <input type="text" class="form-control form-control-lg" id="Surname" placeholder="[Users Surname]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="Dateofbirth">Date of birth</label>
+                        <input type="text" class="form-control form-control-lg" id="Dateofbirth" placeholder="[Users Date of birth]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="SchoolorCollege">School or College</label>
+                        <input type="text" class="form-control form-control-lg" id="SchoolorCollege" placeholder="[Users Institution]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="SchoolYear">School Year</label>
+                        <input type="text" class="form-control form-control-lg" id="SchoolYear" placeholder="[Users School Year]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="Postcode">Postcode</label>
+                        <input type="text" class="form-control form-control-lg" id="Postcode" placeholder="[Users Postcode]">
+                    </div>
+                    <div class="form-group">
+                        <label for="Schoolemailaddress">School email address</label>
+                        <input type="email" class="form-control form-control-lg" id="Schoolemailaddress " placeholder="[Users School email address]" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="Personalemailaddress">Personal email address</label>
+                        <input type="email" class="form-control form-control-lg" id="Personalemailaddress " placeholder="[Users Personal email address]">
+                    </div>
+                    <button type="submit" class="platform-button border-0 t-def mt-5">
+                        Save
+                    </button>
+
+                    
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
   
--- resources/views/frontend/pages/my-account/update-my-preferences.blade.php
+++ resources/views/frontend/pages/my-account/update-my-preferences.blade.php
@@ -1,5 +1,166 @@
 @extends('frontend.layouts.loggedin')
 
 @section('content')
-    update prefernces
+<div class="row p-w">
+    @include('frontend.pages.includes.account-menu')
+    <div class="col-lg-9">
+        <div class="border-left ml-lg-4 pl-lg-5 def-border pb-5">
+            <h1 class="t36 fw700 mb-4">Update my preferences</h1>
+            <p>We try to make sure that you only get the articles that you need and that you are interested in. But sometimes you might change your mind or decide you no longer want to follow a certain path. You can update your preferences using the form below.</p>
+            <div class="row">
+                <div class="col">
+                    <div class="border-top def-border w-100 mb-5"></div>
+                </div>
+            </div>
+            
+            <h2 class="t24 fw700 mb-4">Subjects your interested in</h2>
+
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-header mlg-bg text-center fw700">Like it / Enjoy it /<br>I’m good at it</div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-header vlg-bg text-center fw700">I don’t mind it /<br>50/50 / It’s ok</div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-header mlg-bg text-center fw700">It’s not for me</div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-header vlg-bg text-center fw700">Not applicable /<br>I don’t study that</div></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12"><div class="border-bottom def-border w-100"></div></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 py-2"><div class="fw700 t18 py-2">Subject name A</div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name A" id="inlineRadio1" value="option1"><label for="inlineRadio1"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name A" id="inlineRadio2" value="option2"><label for="inlineRadio2"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name A" id="inlineRadio3" value="option3"><label for="inlineRadio3"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name A" id="inlineRadio4" value="option4"><label for="inlineRadio4"></label></div></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12"><div class="border-bottom gg-border w-100"></div></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 py-2"><div class="fw700 t18">Subject name B</div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name B" id="inlineRadio5" value="option1"><label for="inlineRadio5"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name B" id="inlineRadio6" value="option2"><label for="inlineRadio6"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name B" id="inlineRadio7" value="option3"><label for="inlineRadio7"></label></div></div>
+                        <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-center d-flex justify-content-center align-items-center"><input type="radio" name="Subject name B" id="inlineRadio8" value="option4"><label for="inlineRadio8"></label></div></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12"><div class="border-bottom gg-border w-100"></div></div>
+                    </div>
+
+
+
+                </div>
+            </div>
+                
+            <div class="row my-5">
+                <div class="col">
+                    <div class="border-top def-border w-100 mb-5"></div>
+                </div>
+            </div>
+            <h2 class="t24 fw700 mb-4">Sectors your interested in working in</h2>
+        
+            <div class="row">
+                <div class="col-lg-12"><div class="border-bottom def-border w-100"></div></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Sector A</div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="SectorA" id="Sectors-SectorA" value="SectorA"><label for="Sectors-SectorA"></label></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+            
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Sector B</div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="SectorB" id="Sectors-SectorB" value="SectorB"><label for="Sectors-SectorB"></label></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+
+            <div class="row my-5">
+                <div class="col">
+                    <div class="border-top def-border w-100 mb-5"></div>
+                </div>
+            </div>
+
+            <h2 class="t24 fw700 mb-4">Your possible future options</h2>
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Apprenticeships</div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Apprenticeships" id="Routes-Apprenticeships" value="Apprenticeships"><label for="Routes-Apprenticeships"></label></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+            
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Employment and self-employment </div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Employment" id="Routes-Employment" value="Employment"><label for="Routes-Employment"></label></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+            
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Full-time education </div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Full" id="Routes-Full" value="Full"><label for="Routes-Full"></label></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Higher education </div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Higher" id="Routes-Higher" value="Higher"><label for="Routes-Higher"></label></div>
+                </div>    
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Traineeships and training </div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Traineeships" id="Routes-Traineeships" value="Traineeships"><label for="Routes-Traineeships"></label></div>
+                </div>    
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4"><div class="fw700 t18 p-2 d-inline-block mr-2">Not sure</div></div>
+                <div class="col-lg-6">
+                    <div class="routes-answer"><input type="checkbox" name="Not sure" id="Routes-Notsure" value="Not sure"><label for="Routes-Notsure"></label></div>
+                </div>    
+            </div>
+            <div class="row">
+                <div class="col-lg-10"><div class="border-bottom gg-border w-100"></div></div>
+            </div>
+
+            <button type="submit" class="platform-button border-0 t-def mt-5">
+                Update your preferences
+            </button>
+
+        </div>
+    </div>
+</div>
 @endsection
