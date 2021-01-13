6b275b40fde5e74dbf3df66d3ab539c562f02d24
Fredericld <fred@rfmedia.co.uk>
2021-01-13 09:54:35 +0000

    added logged in check

--- app/Http/Composers/Frontend/AdvisorDetailsComposer.php
+++ app/Http/Composers/Frontend/AdvisorDetailsComposer.php
@@ -17,24 +17,29 @@ class AdvisorDetailsComposer
     public function compose(View $view)
     {
 
-        //get all admins for the users institution
-        $advisors = Auth::guard('web')->user()->institution->admins;
+        if (Auth::guard('web')->check())
+        {
 
-        $ContactableAdvisors = $advisors->filter(function ($value, $key) {
-            return $value->contact_me == 1;
-        });
+            //get all admins for the users institution
+            $advisors = Auth::guard('web')->user()->institution->admins;
 
-        if ($ContactableAdvisors->count() > 0){
-            $ContactableAdvisor = $ContactableAdvisors->first();
-            $ContactableAdvisorName = $ContactableAdvisor->FullName;
-            $canContactAdvisor = $ContactableAdvisor->contact_me;
-        } else {
-            $ContactableAdvisorName = "";
-            $canContactAdvisor = 0;
+            $ContactableAdvisors = $advisors->filter(function ($value, $key) {
+                return $value->contact_me == 1;
+            });
+
+            if ($ContactableAdvisors->count() > 0){
+                $ContactableAdvisor = $ContactableAdvisors->first();
+                $ContactableAdvisorName = $ContactableAdvisor->FullName;
+                $canContactAdvisor = $ContactableAdvisor->contact_me;
+            } else {
+                $ContactableAdvisorName = "";
+                $canContactAdvisor = 0;
+            }
+
+            $view->with('advisorName', $ContactableAdvisorName);
+            $view->with('canContactAdvisor', $canContactAdvisor);
+
         }
-
-        $view->with('advisorName', $ContactableAdvisorName);
-        $view->with('canContactAdvisor', $canContactAdvisor);
 
     }
 
