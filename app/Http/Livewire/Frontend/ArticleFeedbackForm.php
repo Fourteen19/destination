ff759c4e05c9429ec46b56685f72372111494b74
Fredericld <fred@rfmedia.co.uk>
2021-01-14 14:25:10 +0000

    component setup

---
+++ app/Http/Livewire/Frontend/ArticleFeedbackForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire\Frontend;
+
+use Livewire\Component;
+
+class ArticleFeedbackForm extends Component
+{
+
+
+
+
+
+
+
+    public function render()
+    {
+        return view('livewire.frontend.article-feedback-form');
+    }
+
+}
---
+++ resources/views/livewire/frontend/article-feedback-form.blade.php
@@ -0,0 +1,23 @@
+<div class="row r-base mt-5">
+    <div class="col-12">
+        <div class="mlg-bg p-5">
+            <h3 class="fw700 t36 mb-4">Was this page relevant?</h3>
+            <div class="form-check mb-3">
+            <input class="form-check-input position-relative mr-2" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+            <label class="form-check-label t20 fw700" for="exampleRadios1">
+               Yes - It was relevant to me and helpful
+            </label>
+            </div>
+            <div class="form-check mb-3">
+            <input class="form-check-input position-relative mr-2" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label t20 fw700" for="exampleRadios2">
+                Not at all - it's not what I was after
+            </label>
+            </div>
+            <button type="submit" class="platform-button border-0 t-def">
+               Improve your profile
+            </button>
+        </div>
+
+    </div>
+</div>
