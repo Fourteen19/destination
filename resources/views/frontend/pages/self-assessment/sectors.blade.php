4d4f742f30da9ba9befcdd9ecb979564cd2f29cf
Rick Garnett <rick@rfmedia.co.uk>
2021-01-14 11:21:55 +0000

    Work on self assessment screens for responsive testing

--- resources/views/frontend/pages/self-assessment/sectors.blade.php
+++ resources/views/frontend/pages/self-assessment/sectors.blade.php
@@ -4,14 +4,14 @@
 <section class="p-w">
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-10">
-            <div class="row r-pad">
-                <div class="col-lg-6 offset-1">
+    <div class="col-xl-10">
+            <div class="row justify-content-sm-end justify-content-lg-start r-pad">
+                <div class="col-lg-7 offset-lg-1 order-2 order-lg-1">
                     <h1 class="t36 fw700">Getting to know you: <span class="t-alt">Sectors</span></h1>
                     <p>Finally were going to look at the sort of sectors you might be interested in working in.</p>
 
                 </div>
-                <div class="col-lg-3 offset-1">
+                <div class="col-sm-6 col-lg-3 order-1 mb-5 mb-lg-0 order-lg-2">
                     <div class="self-progress-bar w-100">
                     <svg id="Progress" xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 743.37 61.43">
                     <path class="p-bar" d="M2541.33,1388.99h691.5v14.7h-691.5v-14.7Z" transform="translate(-2507.69 -1364.88)"/>
@@ -28,20 +28,10 @@
 
 
     <div class="row justify-content-center">
-        <div class="col-10">
+        <div class="col-xl-10">
             <div class="row r-pad">
-                <div class="col-lg-8 offset-1">
+                <div class="col-lg-8 offset-lg-1">
                     <p class="t24 fw700">Tick as many options as you are interested in or just the one you know that applies to you.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="col-10">
-            <div class="row r-pad">
-                <div class="col-lg-8 offset-1">
-                    @include('admin.pages.includes.flash-message')
                 </div>
             </div>
         </div>
@@ -50,7 +40,7 @@
     {!! Form::open(array('url' => route('frontend.self-assessment.sectors.update'), 'method' => 'PUT')) !!}
 
     <div class="row justify-content-center">
-        <div class="col-10">
+        <div class="col-xl-10">
 
             <div class="row">
                 <div class="col-lg-10 offset-lg-1"><div class="border-bottom def-border w-100"></div></div>
@@ -60,8 +50,8 @@
             @foreach($sectors as $sector)
 
                 <div class="row">
-                    <div class="col-lg-4 offset-lg-1"><div class="fw700 t18 p-2 d-inline-block mr-2">{{$sector->name}}</div><a data-toggle="collapse" data-target="#collapse-{{$sector->slug}}" href="#collapse-{{$sector->slug}}" role="button" aria-expanded="false" aria-controls="collapse-{{$sector->slug}}" class="self-help">?</a></div>
-                    <div class="col-lg-5">
+                    <div class="col-lg-5 offset-lg-1 col-md-6 col-10 pr-0 pr-lg-3 py-2 py-md-0"><div class="fw700 t18 p-md-2 d-inline d-md-inline-block mr-2">{{$sector->name}}</div><a data-toggle="collapse" data-target="#collapse-{{$sector->slug}}" href="#collapse-{{$sector->slug}}" role="button" aria-expanded="false" aria-controls="collapse-{{$sector->slug}}" class="self-help">?</a></div>
+                    <div class="col-lg-5 col-2">
                         <div class="routes-answer">{!! Form::checkbox('sectors[]', $sector->name, ($userSectorTags->where("id", $sector->id)->where("type", 'sector'))->count() == 1 ? true : false, ['id' => $sector->name]) !!}<label for="{{$sector->name}}"></label></div>
                     </div>
                 </div>
@@ -80,9 +70,9 @@
     </div>
 
     <div class="row justify-content-center mt-4">
-        <div class="col-10">
-            <div class="row r-pad">
-                <div class="col-lg-6 offset-1">
+                <div class="col-xl-10">
+                    <div class="row r-pad">
+                        <div class="col-lg-6 offset-lg-1">
                     
                     {!! Form::submit('Previous', ["name" => "submit", "value" => "previous", "class" => "platform-button pb-previous mr-3"]) !!}
                     {!! Form::submit('Next', ["name" => "submit", "value" => "next", "class" => "platform-button pb-next"]) !!}
--- resources/views/frontend/pages/self-assessment/subjects.blade.php
+++ resources/views/frontend/pages/self-assessment/subjects.blade.php
@@ -4,14 +4,14 @@
 <section class="p-w">
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-10">
-            <div class="row r-pad">
-                <div class="col-lg-6 offset-1">
+    <div class="col-xl-10">
+            <div class="row justify-content-sm-end justify-content-lg-start r-pad">
+                <div class="col-lg-7 offset-lg-1 order-2 order-lg-1">
                     <h1 class="t36 fw700">Getting to know you: <span class="t-alt">Subjects</span></h1>
                     <p>Next we-re going to look at the subjects that you enjoy and are good at, compared to those that you are less interested in or don’t want to study in the future.</p>
 
                 </div>
-                <div class="col-lg-3 offset-1">
+                <div class="col-sm-6 col-lg-3 order-1 mb-5 mb-lg-0 order-lg-2">
                     <div class="self-progress-bar w-100">
                     <svg id="Progress" xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 743.37 61.43">
                     <path class="p-bar" d="M2541.33,1388.99h691.5v14.7h-691.5v-14.7Z" transform="translate(-2507.69 -1364.88)"/>
@@ -26,46 +26,37 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-10">
+        <div class="col-xl-10">
             <div class="row r-pad">
-                <div class="col-lg-8 offset-1">
+                <div class="col-lg-8 offset-lg-1">
                     <p class="t24 fw700">Rate each subject using the scale provided. If you don’t currently study that subject then simply mark it as not applicable.</p>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row justify-content-center">
-        <div class="col-10">
-            <div class="row r-pad">
-                <div class="col-lg-8 offset-1">
-                    @include('admin.pages.includes.flash-message')
-                </div>
-            </div>
-        </div>
-    </div>
 
     {!! Form::open(array('url' => route('frontend.self-assessment.subjects.update'), 'method' => 'PUT')) !!}
 
     <div class="row justify-content-center">
-        <div class="col-10">
-            <div class="row">
+        <div class="col-xl-10">
+            <div class="row d-none d-lg-flex">
                 <div class="col-lg-2 offset-lg-1"></div>
                 <div class="col-lg-2 d-flex"><div class="subjects-header mlg-bg text-center fw700">Like it / Enjoy it /<br>I’m good at it</div></div>
                 <div class="col-lg-2 d-flex"><div class="subjects-header vlg-bg text-center fw700">I don’t mind it /<br>50/50 / It’s ok</div></div>
                 <div class="col-lg-2 d-flex"><div class="subjects-header mlg-bg text-center fw700">It’s not for me</div></div>
                 <div class="col-lg-2 d-flex"><div class="subjects-header vlg-bg text-center fw700">Not applicable /<br>I don’t study that</div></div>
             </div>
-            <div class="row">
+            <div class="row d-none d-md-flex">
                 <div class="col-lg-10 offset-lg-1"><div class="border-bottom def-border w-100"></div></div>
             </div>
             @foreach($tagsSubjects as $key => $item)
-                <div class="row">
+                <div class="row mt-3 mt-lg-0">
                     <div class="col-lg-2 offset-lg-1"><div class="fw700 t18 py-2">{{ $item->name }}</div></div>
-                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center">{!! Form::radio("subjects[$item->name]", 'I like it', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 1) ? true : false) : false, ['id' => "subjects[$item->name]['I like it']"]) !!}<label for="subjects[{{ $item->name }}]['I like it']"></label></div></div>
-                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center">{!! Form::radio("subjects[$item->name]", 'I dont mind it', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 2) ? true : false) : false, ['id' => "subjects[$item->name]['I dont mind it']"]) !!}<label for="subjects[{{ $item->name }}]['I dont mind it']"></label></div></div>
-                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center">{!! Form::radio("subjects[$item->name]", 'Not for me', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 3) ? true : false) : false, ['id' => "subjects[$item->name]['Not for me']"]) !!}<label for="subjects[{{ $item->name }}]['Not for me']"></label></div></div>
-                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-center d-flex justify-content-center align-items-center">{!! Form::radio("subjects[$item->name]", 'Not applicable', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 4) ? true : false) : false, ['id' => "subjects[$item->name]['Not applicable']"]) !!}<label for="subjects[{{ $item->name }}]['Not applicable']"></label></div></div>
+                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-md-center d-flex justify-content-lg-center align-items-center p-2 p-lg-0">{!! Form::radio("subjects[$item->name]", 'I like it', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 1) ? true : false) : false, ['id' => "subjects[$item->name]['I like it']"]) !!}<label for="subjects[{{ $item->name }}]['I like it']"></label><label class="mb-label ml-2 d-inline d-lg-none" for="subjects[{{ $item->name }}]['I like it']">Like it / Enjoy it / I’m good at it</label></div></div>
+                    <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-md-center d-flex justify-content-lg-center align-items-center p-2 p-lg-0">{!! Form::radio("subjects[$item->name]", 'I dont mind it', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 2) ? true : false) : false, ['id' => "subjects[$item->name]['I dont mind it']"]) !!}<label for="subjects[{{ $item->name }}]['I dont mind it']"></label><label class="mb-label ml-2 d-inline d-lg-none" for="subjects[{{ $item->name }}]['I dont mind it']">I don’t mind it / 50/50 / It’s ok</label></div></div>
+                    <div class="col-lg-2 d-flex"><div class="subjects-answer mlg-bg text-md-center d-flex justify-content-lg-center align-items-center p-2 p-lg-0">{!! Form::radio("subjects[$item->name]", 'Not for me', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 3) ? true : false) : false, ['id' => "subjects[$item->name]['Not for me']"]) !!}<label for="subjects[{{ $item->name }}]['Not for me']"></label><label class="mb-label ml-2 d-inline d-lg-none" for="subjects[{{ $item->name }}]['Not for me']">It’s not for me</label></div></div>
+                    <div class="col-lg-2 d-flex"><div class="subjects-answer vlg-bg text-md-center d-flex justify-content-lg-center align-items-center p-2 p-lg-0">{!! Form::radio("subjects[$item->name]", 'Not applicable', (isset($userSubjectTags[$item->id])) ? ( ($userSubjectTags[$item->id] == 4) ? true : false) : false, ['id' => "subjects[$item->name]['Not applicable']"]) !!}<label for="subjects[{{ $item->name }}]['Not applicable']"></label><label class="mb-label ml-2 d-inline d-lg-none" for="subjects[{{ $item->name }}]['Not applicable']">Not applicable / I don’t study that</label></div></div>
                 </div>
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1"><div class="border-bottom gg-border w-100"></div></div>
@@ -74,9 +65,9 @@
         </div>
     </div>
             <div class="row justify-content-center mt-4">
-                <div class="col-10">
+                <div class="col-xl-10">
                     <div class="row r-pad">
-                        <div class="col-lg-6 offset-1">
+                        <div class="col-lg-6 offset-lg-1">
                             
                             {!! Form::submit('Previous', ["name" => "submit", "value" => "previous", "class" => "platform-button pb-previous mr-3"]) !!}
                             {!! Form::submit('Next', ["name" => "submit", "value" => "next", "class" => "platform-button pb-next"]) !!}
