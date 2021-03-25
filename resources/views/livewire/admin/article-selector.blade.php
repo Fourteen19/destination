849cdbc589897bf7de9022ca1af7e9e977b30fd2
Rick Garnett <rick@rfmedia.co.uk>
2021-03-25 12:08:51 +0000

    Formating on file selectors

--- resources/views/livewire/admin/article-selector.blade.php
+++ resources/views/livewire/admin/article-selector.blade.php
@@ -1,21 +1,14 @@
-<div class="relative" x-data="{ showDropdown: @entangle('showDropdown') }" @click.away="showDropdown = false">
-
-
-    {{-- <div class="form-group">
-                {!! Form::label('suplink1', 'Free article - slot 1'); !!}
-                {!! Form::select('suplink1', $this->contentList, (!empty($freeArticlesSlot1Page)) ? $freeArticlesSlot1Page : '', array('class' => 'form-control', 'wire:model.lazy' => 'freeArticlesSlot1Page')) !!}
-                @error('freeArticlesSlot1Page') <div class="text-danger error">{{ $message }}</div>@enderror
-            </div> --}}
+<div class="position-relative mb-4" x-data="{ showDropdown: @entangle('showDropdown') }" @click.away="showDropdown = false">
 
     {!! Form::label('suplink1', $label); !!}
 
-    <div class="relative" >
+    <div class="position-relative" >
         @error('freeArticlesSlot1Page') <div class="text-danger error">{{ $message }}</div>@enderror
         <input
             type="text"
             name="{{ $name }}"
             id="{{ $name }}"
-            class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            class="position-relative article-search pl-3 py-2 text-left cursor-default"
             placeholder="Search Article..."
             wire:model="query"
             wire:click="reset"
@@ -26,10 +19,9 @@
             wire:keydown.enter.prevent="selectArticle"
         />
 
-   {{--      <input type="hidden" name="{{ $name }}" id="{{ $name }}" wire:model="selectArticleID">
- --}}
+
         @if ($selectedArticle)
-            <a class="absolute cursor-pointer top-2 right-2 text-gray-500" wire:click="reset">
+            <a class="position-absolute cursor-pointer remove-selection" wire:click="reset">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
@@ -38,16 +30,16 @@
     </div>
 
     @if(!empty($query) && $selectedArticle == 0 && $showDropdown)
-        <div class="absolute z-10 bg-white mt-1 w-full border border-gray-300 rounded-md shadow-lg"   x-show="showDropdown">
+        <div class="position-absolute mt-1 article-results" x-show="showDropdown">
             @if (!empty($articles))
                 @foreach($articles as $i => $article)
                     <a
                         wire:click="selectArticle('{{ $i }}')"
-                        class="d-block py-1 px-2 text-sm cursor-pointer hover:bg-blue-50 {{ $highlightIndex === $i ? 'bg-blue-50' : '' }}"
+                        class="d-block py-1 px-2 cursor-pointer highlight-on {{ $highlightIndex === $i ? 'article-highlight' : '' }}"
                     >{{ $article['title'] }}</a>
                 @endforeach
             @else
-                <span class="block py-1 px-2 text-sm">No results!</span>
+                <span class="d-block py-1 px-2">No results!</span>
             @endif
         </div>
     @endif
