f3a4e2fddf8b93880f561f8a1a8dfea5ed100df9
Fredericld <fred@rfmedia.co.uk>
2021-03-29 15:32:34 +0100

    migration fixed

---
+++ database/migrations/2021_03_29_141920_update_contents_with_next_article_to_read_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateContentsWithNextArticleToReadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+
+            $table->foreignId('read_next_article_id')->nullable();
+
+            $table->foreign('read_next_article_id')
+                    ->references('id')
+                    ->on('pages_live')
+                    ->onDelete('SET NULL');
+        });
+
+
+        Schema::table('contents_live', function (Blueprint $table) {
+
+            $table->foreignId('read_next_article_id')->nullable();
+
+            $table->foreign('read_next_article_id')
+                    ->references('id')
+                    ->on('pages_live')
+                    ->onDelete('SET NULL');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            $table->dropForeign(['read_next_article_id']);
+        });
+
+        Schema::table('contents_live', function (Blueprint $table) {
+            $table->dropForeign(['read_next_article_id']);
+        });
+    }
+}
