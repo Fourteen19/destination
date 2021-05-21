05650245d942f154d67ebfb9ab5aaea6b87864d0
Rick Garnett <rick@rfmedia.co.uk>
2021-05-21 16:44:16 +0100

    Changes to cookie bar

--- resources/lang/vendor/cookieConsent/en/texts.php
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Your experience on this site will be improved by allowing cookies. <a href=":cookies-policy-page">Cookie Policy</a>',
+    'message' => '<p class="mb-2">This website uses cookies.  By using this website and agreeing to this policy, you consent to C & K Careers Ltd\'s (C+K Careers\') use of cookies in accordance with the terms of this policy.</p><div class="row"><div class="col text-center"><a href=":cookies-policy-page" class="cookie-button">Cookie Policy</a>',
     'agree' => 'Allow cookies',
 ];
