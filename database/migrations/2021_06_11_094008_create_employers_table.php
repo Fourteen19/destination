2dcad7d6b70c549162d91461f9ecc0a56fad0312
Fredericld <fred@rfmedia.co.uk>
2021-07-13 12:49:35 +0100

    fix migration

--- database/migrations/2021_06_11_094008_create_employers_table.php
+++ database/migrations/2021_06_11_094008_create_employers_table.php
@@ -14,7 +14,7 @@ class CreateEmployersTable extends Migration
     public function up()
     {
         Schema::create('employers', function (Blueprint $table) {
-            $table->bigIncrements();
+            $table->bigIncrements('id');
             $table->uuid('uuid')->unique();
             $table->string('name', 255)->nullable();
             $table->string('slug', 255)->nullable();
