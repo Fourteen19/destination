1e0aa7d07048f620ca7d67c0a0e7cc705934558f
Fredericld <fred@rfmedia.co.uk>
2021-07-28 13:34:23 +0100

    horizon update

--- config/horizon.php
+++ config/horizon.php
@@ -167,7 +167,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['default', 'exports'],
             'balance' => 'auto',
             'maxProcesses' => 10,
             'memory' => 128,
