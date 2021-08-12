7bbdf5b974db386de782ea2d65af7c7ea2669b40
Fredericld <fred@rfmedia.co.uk>
2021-08-12 14:24:51 +0100

    remove personal email

--- resources/views/livewire/frontend/my-account-user-details.blade.php
+++ resources/views/livewire/frontend/my-account-user-details.blade.php
@@ -34,22 +34,21 @@
                 <label for="Schoolemailaddress">School email address</label>
                 <input type="email" class="form-control form-control-lg" id="Schoolemailaddress " placeholder="{{ Auth::guard('web')->user()->email }}" readonly>
             </div>
-            <div class="form-group">
+
+{{--             <div class="form-group">
                 {!! Form::label('personalEmail', 'Personal email address'); !!}
                 {!! Form::text('personalEmail', null, array('name' => 'personal_email', 'id' => 'personal_email', 'placeholder' => 'Personal email address','class' => 'form-control form-control-lg', 'maxlength' => 255, 'wire:model.defer' => 'personalEmail')) !!}
                 @error('personalEmail') <span class="error">{{ $message }}</span> @enderror
             </div>
-
+ --}}
             <div class="form-group">
                 {!! Form::label('password', 'Password'); !!}
-                {{-- {!! Form::password('password', null, array('name' => 'password', 'id' => 'password', 'placeholder' => 'Password','class' => 'form-control form-control-lg', 'maxlength' => 255, 'wire:model' => 'password')) !!} --}}
                 <input type="password" class="form-control" id="password" class="'form-control form-control-lg" maxlength="255" placeholder="New Password" wire:model.defer="password">
                 @error('password') <span class="error">{{ $message }}</span> @enderror
             </div>
 
             <div class="form-group">
                 {!! Form::label('password_confirmation', 'Confirm Password'); !!}
-                {{-- {!! Form::password('password_confirmation', null, array('name' => 'password_confirmation', 'id' => 'password_confirmation', 'placeholder' => 'Confirm Password','class' => 'form-control form-control-lg', 'maxlength' => 255, 'wire:model' => 'password_confirmation')) !!} --}}
                 <input type="password" class="form-control" id="password_confirmation" class="'form-control form-control-lg" maxlength="255" placeholder="Confirm New Password" wire:model.defer="password_confirmation">
                 @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
             </div>
--- resources/views/livewire/frontend/password-reset-form.blade.php
+++ resources/views/livewire/frontend/password-reset-form.blade.php
@@ -4,14 +4,12 @@
 
             <div class="form-group">
                 {!! Form::label('password', 'Enter New Password'); !!}
-                {{-- {!! Form::password('password', null, array('name' => 'password', 'id' => 'password', 'placeholder' => 'Password','class' => 'form-control form-control-lg', 'maxlength' => 255, 'wire:model' => 'password')) !!} --}}
                 <input type="password" class="form-control" id="password" class="'form-control form-control-lg" maxlength="255" placeholder="New Password" wire:model.defer="password">
                 @error('password') <span class="error">{{ $message }}</span> @enderror
             </div>
 
             <div class="form-group">
                 {!! Form::label('password_confirmation', 'Confirm New Password'); !!}
-                {{-- {!! Form::password('password_confirmation', null, array('name' => 'password_confirmation', 'id' => 'password_confirmation', 'placeholder' => 'Confirm Password','class' => 'form-control form-control-lg', 'maxlength' => 255, 'wire:model' => 'password_confirmation')) !!} --}}
                 <input type="password" class="form-control" id="password_confirmation" class="'form-control form-control-lg" maxlength="255" placeholder="Confirm New Password" wire:model.defer="password_confirmation">
                 @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
             </div>
