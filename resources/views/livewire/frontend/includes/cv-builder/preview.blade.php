b83deb35cfba6ccb729920e3faa1c1062f58a56e
Rick Garnett <rick@rfmedia.co.uk>
2021-09-16 17:14:19 +0100

    CV Builder and dashboard updates

--- resources/views/livewire/frontend/includes/cv-builder/preview.blade.php
+++ resources/views/livewire/frontend/includes/cv-builder/preview.blade.php
@@ -4,7 +4,8 @@
 
         @if ($template == 1)
 
-            <h2 class="fw700 t18">You have selected Template 1</h2>
+            <h2 class="fw700 t18">You have selected "Template 1"</h2>
+            <p class="t16">Note: The preview below is for information purposes only and your CV is shown as one continuous document. Your CV may break on to two or more pages when it is downloaded or printed from a PDF. You should not print this screen - to download your final CV, use the "Save & download as PDF" button at the bottom of the screen.</p>
 
             <div class="cv-preview-outer">
                 <div class="cv-preview-inner">
@@ -24,7 +25,7 @@
                     @endif
 
                     <div class="row mt-3">
-                        <div class="col-12 fw600"><div class="cv-inner-heading">Skills and experience</div></div>
+                        <div class="col-12 fw600"><div class="cv-inner-heading">Employment history</div></div>
                     </div>
 
                     @foreach($relatedEmployments as $key => $employment)
