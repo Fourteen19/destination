ec8ea355c26853aa1c74b73fd9fd44373224cdda
Fredericld <fred@rfmedia.co.uk>
2021-09-30 15:30:20 +0100

    TP  admins can create vacnacies & events

---
+++ resources/views/admin/mail/admin-requests-vacancy-action.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.mail.master')
+
+@section('content')
+<div style="background-color: #e0e0e0; padding: 16px; border-radius:6px;">
+    <div style="background-color: #fff; padding: 10px;">
+        <table style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; width:500px;" width="500px" align="center" border="0" cellpadding="5" cellspacing="3" class="inner-table">
+            <tr>
+                <td style="color:#666666; width: 33%; vertical-align: top" valign="top">From:</td>
+                <td style="vertical-align: top" valign="top">{{ Auth::guard('admin')->user()->TitleFullName }}</td>
+            </tr>
+            <tr>
+                <td style="color:#666666; vertical-align: top">Query:</td>
+                <td style="vertical-align: top" valign="top">
+                    @if ($details['action'] == 'make_live')
+                        Could you please make the following vacancy live?
+                    @elseif ($details['action'] == 'remove_live')
+                        Could you please remove the following vacancy from live?
+                    @elseif ($details['action'] == 'delete')
+                        Could you please delete the following vacancy?
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <td style="color:#666666; vertical-align: top">Vacancy:</td>
+                <td style="vertical-align: top" valign="top">{{ ($details['title']) }}</td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+@endsection
--- resources/views/livewire/admin/includes/client-static-content/events.blade.php
+++ resources/views/livewire/admin/includes/client-static-content/events.blade.php
@@ -7,6 +7,12 @@
                 {!! Form::textarea('no_event', $no_event, array('placeholder' => 'Enter fall back text','class' => 'form-control', 'wire:model.defer' => 'no_event')) !!}
             </div>
 
+            <div class="form-group">
+                @error('event_email_notification') <span class="text-danger error">{{ $message }}</span>@enderror
+                {!! Form::label('notifications', 'Please enter a semicolon separated list of administrator emails who should be notified when a third-party admin creates or updates an event'); !!}
+                {!! Form::textarea('notifications', $event_email_notification, array('placeholder' => 'admin1@mydirections.co.uk;admin2@mydirections.co.uk','class' => 'form-control', 'wire:model.defer' => 'event_email_notification')) !!}
+            </div>
+
         </div>
     </div>
 </div>
