f763808beb4a14fb197455bdc1b64cbdac15c635
Rick Garnett <rick@rfmedia.co.uk>
2021-10-06 14:44:34 +0100

    Text updates

--- resources/views/frontend/auth/passwords/email.blade.php
+++ resources/views/frontend/auth/passwords/email.blade.php
@@ -1,47 +1,59 @@
 @extends('frontend.layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card my-5">
-                <div class="card-header"><h1 class="t24 fw700 mb-0">{{ __('Reset Password') }}</h1></div>
 
-                <div class="card-body">
-                    @if (session('status'))
+<div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="p-w">
+                <h1 class="t36 fw700">Forgotten Password</h1>
+                <div class="row">
+                    <div class="col-md-8">
+                        <p>If you would like to reset your password please enter your primary email address below (please make sure you use the original or school email address linked to your account).</p>
+                    </div>
+                </div>
+
+                @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('frontend.password.email') }}">
+
+                <form method="POST" action="{{ route('frontend.password.email') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-8">
+                                <div class="form-group">
+                                    <label for="email" class="col-form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+
+                                        <div class="mt-3 t14">If you have added an alternative email address this will be cc'd in your password retrieval email.</div>
+                                    
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-6">
                                 <button type="submit" class="platform-button border-0">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
                         </div>
                     </form>
-                </div>
+
             </div>
         </div>
     </div>
-</div>
+
+
+
 @endsection
