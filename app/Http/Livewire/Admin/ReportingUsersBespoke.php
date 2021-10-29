97d665083708f45487d65f27d9251096905dc311
Fredericld <fred@rfmedia.co.uk>
2021-10-29 09:54:34 +0100

    build

--- app/Http/Livewire/Admin/ReportingUsersBespoke.php
+++ app/Http/Livewire/Admin/ReportingUsersBespoke.php
@@ -25,13 +25,79 @@ class ReportingUsersBespoke extends Component
     public $message = "";
     public $reportType = "";
     public $displayExportButtons = False;
+    public $tagsSubjects, $tagsYearGroups, $tagsLscs, $tagsRoutes, $tagsSectors, $tagsNeet;
+    public $yesNoOptions = ['0' => 'N/A', 'Y' => 'Yes', 'N' => 'No'];
+    public $tagsYearGroupsSelected = [];
+    public $tagsLscsSelected = [];
+    public $tagsSubjectsSelected = [];
+    public $tagsRoutesSelected = [];
+    public $tagsSectorsSelected = [];
+    public $tagsNeetSelected = [];
+    public $cvCompleted = 0;
+    public $redFlag = 0;
+    public $allYears= 1;
+    public $allCrsYears = 1;
 
     public function mount($reportType)
     {
         $this->reportType = $reportType;
 
         $this->getInstitutionsList();
+
+
+
+        $this->tagsYearGroups = SystemTag::select('uuid', 'name')->where('type', 'year')->get()->toArray();
+        foreach($this->tagsYearGroups as $key => $value){
+            $this->tagsYearGroupsSelected[] = $value['name'][ app()->getLocale() ];
+        }
+
+
+        $this->tagsLscs = SystemTag::select('uuid', 'name')->where('type', 'career_readiness')->where('live', 'Y')->get()->toArray();
+        foreach($this->tagsLscs as $key => $value){
+            $this->tagsLscsSelected[] = $value['name'][ app()->getLocale() ];
+        }
+
+        $this->tagsRoutes = SystemTag::select('uuid', 'name')->where('type', 'route')->where('live', 'Y')->orderBy('name', 'ASC')->get()->toArray();
+        $this->tagsSectors = SystemTag::select('uuid', 'name')->where('type', 'sector')->where('live', 'Y')->orderBy('name', 'ASC')->get()->toArray();
+        $this->tagsSubjects = SystemTag::select('uuid', 'name')->where('type', 'subject')->where('live', 'Y')->orderBy('name', 'ASC')->get()->toArray();
+
+
+        $this->tagsNeet = SystemTag::select('uuid', 'name')->where('type', 'neet')->where('live', 'Y')->orderBy('name', 'ASC')->get()->toArray();
+
     }
+
+
+
+    /**
+     * AllYearsOn
+     * when the "all years" checkbox is selected
+     *
+     * @return void
+     */
+    public function AllYearsOn()
+    {
+        $this->tagsYearGroups = SystemTag::select('uuid', 'name')->where('type', 'year')->get()->toArray();
+
+        $this->tagsYearGroupsSelected = [];
+        foreach($this->tagsYearGroups as $key => $value){
+            $this->tagsYearGroupsSelected[] = $value['name']['en'];
+        }
+
+    }
+
+
+
+    public function AllCrsYearsOn()
+    {
+        $this->tagsLscs = SystemTag::select('uuid', 'name')->where('type', 'career_readiness')->where('live', 'Y')->get()->toArray();
+
+        $this->tagsLscsSelected = [];
+        foreach($this->tagsLscs as $key => $value){
+            $this->tagsLscsSelected[] = $value['name']['en'];
+        }
+
+    }
+
 
 
 
@@ -83,8 +149,42 @@ class ReportingUsersBespoke extends Component
                 $this->displayExportButtons = False;
             }
 
+        } elseif ($propertyName == "allYears"){
+
+            if ($this->allYears == 1){
+                $this->AllYearsOn();
+            }
+
+        } elseif ($propertyName == "allCrsYears"){
+
+            if ($this->allCrsYears == 1){
+                $this->AllCrsYearsOn();
+            }
+
+
+           // dd($this->tagsSubjectsSelected);
+
         }
 
+    }
+
+
+
+
+    public function getBespokeFilters()
+    {
+
+        return [
+            'allYearGroup' => $this->allYears,
+            'yearGroupSelected' => $this->tagsYearGroupsSelected,
+            'allCrsYears' => $this->allCrsYears,
+            'tagsCrsSelected' => $this->tagsLscsSelected,
+            'tagsSubjectsSelected' => $this->tagsSubjectsSelected,
+            'tagsRoutesSelected' => $this->tagsRoutesSelected,
+            'tagsSectorsSelected' => $this->tagsSectorsSelected,
+            'cvCompleted' => $this->cvCompleted,
+            'redFlag' => $this->redFlag,
+        ];
     }
 
 
@@ -110,16 +210,55 @@ class ReportingUsersBespoke extends Component
                 if ($this->reportType == "user-data")
                 {
 
-                    $this->resultsPreview = User::query()->where('institution_id', $institutionId)
-                                                        ->where('type', 'user')
-                                                        ->count();
+                    $filters = $this->getBespokeFilters();
+//dd($filters);
+/*
+array:9 [▼
+  "allYearGroup" => 1
+  "yearGroupSelected" => array:8 [▶]
+  "allCrsYears" => 1
+  "tagsCrsSelected" => array:4 [▶]
+  "tagsSubjectsSelected" => []
+  "tagsRoutesSelected" => []
+  "tagsSectorsSelected" => []
+  "cvCompleted" => 0
+  "redFlag" => 0
+]
+*/
+//dd($filters['tagsRoutesSelected']);
+                    $query = User::query()->where('institution_id', $institutionId)->where('type', 'user');
 
-                } elseif ($this->reportType == "user-not-logged-in-data") {
+                    $query = $query->whereIn('school_year', $filters['yearGroupSelected']);
+//dd($filters['yearGroupSelected']);
+                    $query = $query->wherehas('selfAssessment', function ($query) use ($filters) {
 
-                    $this->resultsPreview = User::query()->where('institution_id', $institutionId)
-                                                        ->where('nb_logins', 0)
-                                                        ->where('type', 'user')
-                                                        ->count();
+                                //the assessment Must have the same year as the user's school year
+                                $query->whereRaw('self_assessments.year = users.school_year');
+
+                                if (count($filters['tagsRoutesSelected']) > 0)
+                                {
+                                    $query->withAllTags($filters['tagsRoutesSelected'], 'route');
+                                }
+
+                                if (count($filters['tagsRoutesSelected']) > 0)
+                                {
+                                    $query->withAllTags($filters['tagsRoutesSelected'], 'route');
+                                }
+
+
+
+
+                    });
+/*
+                    //$query = $query->withAllTags(['tag 1', 'tag 2'], 'myType')
+*/
+                    if ($filters['cvCompleted'] != 0)
+                    {
+                        $query = $query->where('cv_builder_completed', $filters['cvCompleted']);
+                    }
+
+dd($query->get());
+                    $this->resultsPreview = $query->count();
 
                 }
 
@@ -194,15 +333,6 @@ class ReportingUsersBespoke extends Component
 
                         //runs the export
                         (new UsersExport( session()->get('adminClientSelectorSelected'), $institution->id))->queue($filename, 'exports')->chain([
-                            new NotifyUserOfCompletedExport(request()->user(), $filename),
-                        ]);
-
-                    } elseif ($this->reportType == "user-not-logged-in-data") {
-
-                        $filename = 'user-not-logged-in_'.Str::slug($this->institutionName).'_'.date("dmyHis").'.csv';
-
-                        //runs the export
-                        (new UsersNotLoggedInExport( session()->get('adminClientSelectorSelected'), $institution->id))->queue($filename, 'exports')->chain([
                             new NotifyUserOfCompletedExport(request()->user(), $filename),
                         ]);
 
--- resources/views/livewire/admin/reporting-users-bespoke.blade.php
+++ resources/views/livewire/admin/reporting-users-bespoke.blade.php
@@ -10,6 +10,117 @@
         </select>
     </div>
 
+
+
+    <div class="form-group">
+        {!! Form::label('tagsYearGroups', 'Year Groups', ['class' => 'filter-header']); !!}
+
+        <div class="form-check form-select-all">
+            {!! Form::checkbox('all_years', true, false, ['class' => 'form-check-input', 'id' => 'all_years', 'wire:model' => 'allYears' ]) !!}
+            <label class="form-check-label" for="all_years">All years</label>
+        </div>
+
+        @foreach($tagsYearGroups as $tag)
+            <div class="form-check">
+            <input class="form-check-input" id="{{$tag['uuid']}}" @if ($allYears) disabled @endif wire:model="tagsYearGroupsSelected" name="tagsYearGroups[]" type="checkbox" value="{{$tag['name'][app()->getLocale()]}}">
+            <label class="form-check-label" for="{{$tag['uuid']}}">
+                @if ($tag['name'][app()->getLocale()] != 14) {{$tag['name'][app()->getLocale()]}} @else Post @endif
+            </label>
+
+            </div>
+        @endforeach
+
+    </div>
+
+
+
+    <div class="form-group">
+        {!! Form::label('tagsLscs', 'Careers Readiness Score', ['class' => 'filter-header']); !!}
+
+        <div class="form-check form-select-all">
+            {!! Form::checkbox('all_crs_years', true, false, ['class' => 'form-check-input', 'id' => 'all_crs_years', 'wire:model' => 'allCrsYears' ]) !!}
+            <label class="form-check-label" for="all_crs_years">All CRS scrores</label>
+        </div>
+
+        @foreach($tagsLscs as $tag)
+            <div class="form-check">
+                <input class="form-check-input" id="{{$tag['uuid']}}" @if ($allCrsYears) disabled @endif wire:model="tagsLscsSelected" name="tagsLscs[]" type="checkbox" value="{{$tag['name'][app()->getLocale()]}}">
+                <label class="form-check-label" for="{{$tag['uuid']}}">{{$tag['name'][app()->getLocale()]}}</label>
+            </div>
+        @endforeach
+    </div>
+
+
+
+
+
+
+    <div class="form-group">
+        {!! Form::label('tagsSubjects', 'Subjects', ['class' => 'filter-header']); !!}
+
+        @foreach($tagsSubjects as $tag)
+            <div class="form-check">
+                {!! Form::checkbox('tagsSubjects[]', $tag['name'][app()->getLocale()], false, ['class' => 'form-check-input', 'id' => $tag['uuid'], 'wire:model' => 'tagsSubjectsSelected' ]) !!}
+                <label class="form-check-label" for="{{$tag['uuid']}}">{{$tag['name'][app()->getLocale()]}}</label>
+            </div>
+        @endforeach
+
+    </div>
+
+
+
+    <div class="form-group">
+        {!! Form::label('tagsRoutes', 'Routes', ['class' => 'filter-header']); !!}
+
+        @foreach($tagsRoutes as $tag)
+            <div class="form-check">
+                {!! Form::checkbox('tagsRoutes[]', $tag['name'][app()->getLocale()], false, ['class' => 'form-check-input', 'id' => $tag['uuid'], 'wire:model' => 'tagsRoutesSelected' ]) !!}
+                <label class="form-check-label" for="{{$tag['uuid']}}">{{$tag['name'][app()->getLocale()]}}</label>
+            </div>
+        @endforeach
+
+    </div>
+
+
+
+    <div class="form-group">
+        {!! Form::label('tagsSectors', 'Sectors', ['class' => 'filter-header']); !!}
+
+        @foreach($tagsSectors as $tag)
+            <div class="form-check">
+                {!! Form::checkbox('tagsSectors[]', $tag['name'][app()->getLocale()], false, ['class' => 'form-check-input', 'id' => $tag['uuid'], 'wire:model' => 'tagsSectorsSelected' ]) !!}
+                <label class="form-check-label" for="{{$tag['uuid']}}">{{$tag['name'][app()->getLocale()]}}</label>
+            </div>
+        @endforeach
+
+    </div>
+
+
+
+
+    <div class="form-group">
+        {!! Form::label('cvCompleted', 'CV Builder Completed', ['class' => 'filter-header']); !!}
+        <select class="form-control" id="cv_completed" name="cv_completed" wire:model="cvCompleted">
+            @foreach($yesNoOptions as $optionValue => $optionLabel)
+                <option value="{{$optionValue}}">{{$optionLabel}}</option>
+            @endforeach
+        </select>
+
+    </div>
+
+
+
+
+    <div class="form-group">
+        {!! Form::label('red_flag', 'Red Flag', ['class' => 'filter-header']); !!}
+        <select class="form-control" id="red_flag" name="red_flag" wire:model="redFlag">
+            @foreach($yesNoOptions as $optionValue => $optionLabel)
+                <option value="{{$optionValue}}">{{$optionLabel}}</option>
+            @endforeach
+        </select>
+
+    </div>
+
     @if ($displayExportButtons == 1)
 
         <button type="button" class="btn mydir-button" wire:click="checkResults" wire:loading.attr="disabled">Check results</button>
