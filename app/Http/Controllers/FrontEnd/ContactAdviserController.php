d6232e0316eabf648d06f44f03583f855cc0bedb
Fredericld <fred@rfmedia.co.uk>
2021-11-13 15:42:04 +0000

    fix for multiple advisers

--- app/Http/Controllers/FrontEnd/ContactAdviserController.php
+++ app/Http/Controllers/FrontEnd/ContactAdviserController.php
@@ -23,19 +23,19 @@ class ContactAdviserController extends Controller
 
         SEOMeta::setTitle("Contact my adviser");
 
-        $redirect = 0;
+        $redirect = 1;
 
-        $institutionAdvisor = Auth::guard('web')->user()->institution->admins->first();
-        if ($institutionAdvisor)
+        $institutionAdvisors = Auth::guard('web')->user()->institution->admins()->role( config('global.admin_user_type.Advisor') )->get();
+
+        foreach ($institutionAdvisors as $institutionAdvisor)
         {
-            if ($institutionAdvisor->contact_me != 'Y')
-            {
-                $redirect = 1;
-            }
-        } else {
-            $redirect = 1;
-        }
 
+            if ($institutionAdvisor->contact_me == 'Y')
+            {
+                $redirect = 0;
+            }
+
+        }
 
         if ($redirect == 1)
         {
